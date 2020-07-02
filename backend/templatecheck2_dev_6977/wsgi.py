12ed52efd53a33cfab549f116d48f97f77496a7e
Crowdbotics <team@crowdbotics.com>
2020-07-02 19:38:58 +0000

    Initial commit.

---
+++ backend/templatecheck2_dev_6977/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for templatecheck2_dev_6977 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'templatecheck2_dev_6977.settings')
+
+application = get_wsgi_application()
